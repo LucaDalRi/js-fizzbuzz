ddac7a9c10a178693f4596c4e0aca5c68cdd8772
LucaDalRi <euro2xd@gmail.com>
2022-12-21 16:24:10 +0100

    Prima prova funzionante

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+/* Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100, 
+ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+e per i multipli di 5 stampi “Buzz”. 
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+
+for (let i = 0; i <= 100;  i++) {
+
+    if (i % 5 == 0 && i % 3 == 0) {
+
+        console.log('FizzBuzz');
+
+    }
+    else if (i % 3 == 0) {
+
+        console.log('Fizz');
+
+    }
+    else if (i % 5 == 0) {
+
+        console.log('Buzz');
+
+    }
+    else{
+
+        console.log(i);
+
+    }
+
+
+}
